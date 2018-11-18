7f08586661684bb92df82aa2f1939d3bab9f31ea
Tom Freudenberg <tom.freudenberg@4commerce.de>
2018-11-18 15:52:58 +0100

    mod: enable controller for template and check button1 state in console log

--- imports/ui/components/controller/index.js
+++ imports/ui/components/controller/index.js
@@ -5,31 +5,39 @@ import './controller.html';
 
 Template.controller.onCreated(function () {
 
-  var win_width = $(window).width();
-  var win_height = $(window).height();
-
-  this.joystick = new VirtualJoystick({
-    container: document.getElementById('container'),
-    stickElement: document.getElementById('virtualjoystick'),
-    baseElement: document.getElementById('virtualjoystick'),
-    stationaryBase: true,
-    baseX: win_width / 2,
-    baseY: win_height / 2,
-    limitStickTravel: true,
-    stickRadius: 200,
-    add1Button: true,
-    button1X: 50 + 25,
-    button1Y: win_height - 50 - 25,
-    mouseSupport: true,
-  });
 });
 
 Template.controller.onRendered(function() {
   console.log('Given parameter for controller: ' + Template.instance().data.controller);
+
+  // run rendering of joystick when template is ready
+  this.autorun(() => {
+    // get dimensions from window
+    var win_width = $(window).width();
+    var win_height = $(window).height();
+    // create joystick controller
+    this.joystick = new VirtualJoystick({
+      container: document.getElementById('virtualjoystick'),
+      stationaryBase: true,
+      baseX: win_width / 2,
+      baseY: win_height / 2,
+      limitStickTravel: true,
+      stickRadius: 200,
+      add1Button: true,
+      button1X: 50 + 25,
+      button1Y: win_height - 50 - 25,
+      mouseSupport: true,
+    });
+  });
 });
 
 Template.controller.helpers({
+
 });
 
 Template.controller.events({
+  'mousedown/touchstart': function(event, template) {
+    // check button event when clicked or pressed
+    console.log('Button1 pressed: ' + Template.instance().joystick.button1Pressed);
+  }
 });
