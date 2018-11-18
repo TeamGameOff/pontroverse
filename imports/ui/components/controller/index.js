a07ea31ad03792a6a21f7d3c95fbe0abe33b9148
Tom Freudenberg <tom.freudenberg@4commerce.de>
2018-11-18 04:08:04 +0100

    add: virtualjoystick for controller support

--- imports/ui/components/controller/index.js
+++ imports/ui/components/controller/index.js
@@ -1,8 +1,27 @@
 import { Template } from 'meteor/templating';
+import { VirtualJoystick } from '../../../lib/client/virtualjoystick';
 
 import './controller.html';
 
 Template.controller.onCreated(function () {
+
+  var win_width = $(window).width();
+  var win_height = $(window).height();
+
+  this.joystick = new VirtualJoystick({
+    container: document.getElementById('container'),
+    stickElement: document.getElementById('virtualjoystick'),
+    baseElement: document.getElementById('virtualjoystick'),
+    stationaryBase: true,
+    baseX: win_width / 2,
+    baseY: win_height / 2,
+    limitStickTravel: true,
+    stickRadius: 200,
+    add1Button: true,
+    button1X: 50 + 25,
+    button1Y: win_height - 50 - 25,
+    mouseSupport: true,
+  });
 });
 
 Template.controller.onRendered(function() {
